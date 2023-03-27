bd702d7c337e606191bf9c631f84cc77f1a1268a
creallfluharty <creallfluharty@gmail.com>
2023-03-26 20:17:05 -0500

    fix pixel coordinate translation issue

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -6,8 +6,8 @@
 Canvas::Canvas(uint32_t width, uint32_t height) :
     width(width),
     height(height),
-    x_offset(-static_cast<int32_t>(width / 2)),
-    y_offset(-static_cast<int32_t>(height / 2)),
+    x_offset(static_cast<int32_t>(width / 2)),
+    y_offset(static_cast<int32_t>(height / 2)),
     window(
         SDL_CreateWindow(
             "CGFromScratch",
@@ -21,22 +21,22 @@ Canvas::Canvas(uint32_t width, uint32_t height) :
         SDL_CreateRenderer(this->window.get(), -1, 0),
         SDL_DestroyRenderer
     ) {
-        if (!this->window) {
+        if (!this->window)
             throw std::runtime_error(SDL_GetError());
-        }
 
-        if (!this->renderer) {
+        if (!this->renderer)
             throw std::runtime_error(SDL_GetError());
-        }
     }
 
 void Canvas::put_pixel(int32_t x, int32_t y, Color color) {
     if (this->get_left() > x || x > this->get_right() || this->get_bottom() > y || y > this->get_top())
         throw std::out_of_range("Pixel location out of range.");
-    
+
     SDL_SetRenderDrawColor(this->renderer.get(), color.get_red(), color.get_green(), color.get_blue(), Color::get_channel_max());
 
-    SDL_RenderDrawPoint(this->renderer.get(), x - this->x_offset, -y - this->y_offset);
+    auto inverted_y = -(y + 1); // + 1 since (x, y) refers to the bottom-left of the pixel rather than the top-left
+
+    SDL_RenderDrawPoint(this->renderer.get(), x + this->x_offset, inverted_y + this->y_offset);
 }
 
 void Canvas::clear(Color color) {
@@ -52,10 +52,10 @@ uint32_t Canvas::get_width() const { return this->width; }
 
 uint32_t Canvas::get_height() const { return this->height; }
 
-int32_t Canvas::get_left() const { return this->x_offset; }
+int32_t Canvas::get_left() const { return -this->x_offset; }
 
-int32_t Canvas::get_right() const { return this->width + this->x_offset - 1; }
+int32_t Canvas::get_right() const { return this->width - this->x_offset - 1; }
 
-int32_t Canvas::get_bottom() const { return this->y_offset; }
+int32_t Canvas::get_bottom() const { return -this->y_offset; }
 
-int32_t Canvas::get_top() const { return this->height + this->y_offset - 1; }
+int32_t Canvas::get_top() const { return this->height - this->y_offset - 1; }
